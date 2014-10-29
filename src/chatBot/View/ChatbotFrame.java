f12ad0cc11bcb8a0a417deddfdaf7fb9c9406bd9
BryonAttaway <DrakonUp@gmail.com>
2014-10-29 09:53:32 -0600

    added another button

--- src/chatBot/View/ChatbotFrame.java
+++ src/chatBot/View/ChatbotFrame.java
@@ -8,6 +8,10 @@ public class ChatbotFrame extends JFrame
 {
 	private ChatbotPanel basePanel;
 	
+	/**
+	 * creates a chatbot panel called baseController
+	 * @param baseController is the way you get from the user to do stuff
+	 */
 	public ChatbotFrame(ChatBotAppController baseController) 
 	{
 		basePanel = new ChatbotPanel(baseController);
@@ -16,8 +20,25 @@ public class ChatbotFrame extends JFrame
 	
 	private void setupFrame()
 	{
+		/**
+		 * this is what makes up the window
+		 */
 		this.setContentPane(basePanel);
-		this.setSize(400, 400);
+		/**
+		 * sets where on your screen it will pop-up
+		 */
+		this.setLocation(10,10);
+		/**
+		 * sets the size (X,Y)
+		 */
+		this.setSize(500, 400);
+		/**
+		 * The user can't change the size of the window
+		 */
+		this.setResizable(false);
+		/**
+		 * This makes it so you can see it
+		 */
 		this.setVisible(true);
 	}
 
--- src/chatBot/View/ChatbotPanel.java
+++ src/chatBot/View/ChatbotPanel.java
@@ -1,35 +1,93 @@
 package chatBot.View;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 import chatBot.Controller.ChatBotAppController;
 
 public class ChatbotPanel extends JPanel
-{
+{	
 	private ChatBotAppController baseController;
+	private JButton sampleButton;	
+	private JButton nextButton;
+	private JTextField sampleField;	
+	private JTextArea chatArea;
+	private JScrollPane chatPane;
+	private SpringLayout baseLayout;	
 	
 	public ChatbotPanel(ChatBotAppController baseController) 
 	{
 		this.baseController = baseController;
 		
+		sampleButton = new JButton("click on me, or DIE >:)");
+		nextButton = new JButton("Or click on me :D");
+		sampleField = new JTextField(25);
+		chatArea = new JTextArea(5, 25);
+		chatArea.setTabSize(5);
+		chatPane = new JScrollPane(chatArea);
+		
+		baseLayout = new SpringLayout();
+
+
+		
+		setupScrollPane();				
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
 	
+	private void setupScrollPane()
+	{
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+	}
+	
 	private void setupPanel()
 	{
+		this.setBackground(Color.CYAN);
+		this.setLayout(baseLayout);
 		
+		this.add(sampleButton);
+		this.add(sampleField);
+		this.add(chatPane);	
+		add(nextButton);
 	}
-	
+		
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, chatArea, 106, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, chatArea, 10, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, sampleButton, 42, SpringLayout.EAST, chatPane);
+		baseLayout.putConstraint(SpringLayout.SOUTH, sampleButton, 0, SpringLayout.SOUTH, chatPane);
+		baseLayout.putConstraint(SpringLayout.NORTH, chatPane, 100, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, chatPane, 10, SpringLayout.WEST, this);		
+		baseLayout.putConstraint(SpringLayout.SOUTH, chatPane, 200, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, chatPane, 225, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, sampleField, 3, SpringLayout.NORTH, chatPane);
+		baseLayout.putConstraint(SpringLayout.WEST, sampleField, 6, SpringLayout.EAST, chatPane);
+		baseLayout.putConstraint(SpringLayout.NORTH, nextButton, 22, SpringLayout.SOUTH, sampleButton);
+		baseLayout.putConstraint(SpringLayout.WEST, nextButton, 0, SpringLayout.WEST, sampleButton);
 	}
-	
+		
 	private void setupListeners()
 	{
+		sampleButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				sampleField.setText(sampleField.getText() + " :O OMG ");
+			}
+		});
 		
+		nextButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				sampleField.setText(sampleField.getText() + " you clicked on me!!!! ");
+			}
+		});
 	}
-
 }
